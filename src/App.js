6e2c02d8f090e47a3ab4ec2388f780d6e3696a35
Depa Himavanth Reddy <himavanth@cygengroup.com>
2022-03-09 23:19:17 +0530

    configured json server

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import UserRegistration from './pages/UserRegistration';
 function App() {
   return (
-    <p>Hi</p>
+    <Router>
+      <Routes>
+        <Route path=''><UserRegistration /></Route>
+      </Routes>
+    </Router>
   );
 }
 
